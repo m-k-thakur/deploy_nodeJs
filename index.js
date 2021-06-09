59ba7fcb2ed2c619417393f23e055b2817db43df
Manoj Kumar <57798069+m-k-thakur@users.noreply.github.com>
2021-06-09 14:44:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Manoj Kumar Thakur Manoj Kumar Thakur Manoj Kumar Thakur')
+  res.send('Manoj Kumar Thakur Sample Version 4')
 })
 
 app.get('/status', (req, res) => {
